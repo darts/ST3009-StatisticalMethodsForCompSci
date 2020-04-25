060d7f3f5bd85847634ae67685f8f6d47765a2a5
darts <darts@tcd.ie>
2020-04-25 18:04:50 +0100

    Create Q1_f.m

---
+++ FinalExam/Q1_f.m
@@ -0,0 +1,22 @@
+extSim(5000)
+function X = stoSim3 (n)
+    choice = randperm(10); %get elements in random order
+    choice = choice(1:3); %select 3 questions for exam
+    studied = randperm(10); %random order of studied qs
+    studied = studied(1:n); %n number of studied qs
+    overlap = setdiff(choice, studied); %studied vs exam
+    if numel(overlap) < 2 %if at least 2 studied appear
+        X = 1;
+    else
+        X = 0;
+    end
+end
+
+function Y = extSim(N)
+    generated = randi(11,[1,N])-1;
+    Y = 0;
+    for i = 1:numel(generated)
+        Y = Y + stoSim3(generated(i));
+    end
+    Y = Y/N;
+end
