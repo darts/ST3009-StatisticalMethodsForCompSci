fca2f9c4c8d0c5be25a22269feb68741e1791b2a
darts <darts@tcd.ie>
2020-04-25 17:41:35 +0100

    Update Q1_d.m
    Comments

--- FinalExam/Q1_d.m
+++ FinalExam/Q1_d.m
@@ -1,11 +1,11 @@
-stoSim3(10)
+stoSim3(1)
 function X = stoSim3 (n)
-    choice = randperm(10);
-    choice = choice(1:3);
-    studied = randperm(10);
-    studied = studied(1:n);
-    overlap = setdiff(choice, studied);
-    if numel(overlap) < 2
+    choice = randperm(10); %get elements in random order
+    choice = choice(1:3); %select 3 questions for exam
+    studied = randperm(10); %random order of studied qs
+    studied = studied(1:n); %n number of studied qs
+    overlap = setdiff(choice, studied); %studied vs exam
+    if numel(overlap) < 2 %if at least 2 studied appear
         X = 1;
     else
         X = 0;
